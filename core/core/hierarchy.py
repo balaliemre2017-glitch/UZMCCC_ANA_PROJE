2ae5e605af318233fe420c31a375cfda956e146c
balaliemre2017-glitch <balaliemre2017@gmail.com>
2026-08-11 13:26:31 +0300

    Implement HierarchyManager class for department management

---
+++ core/core/hierarchy.py
@@ -0,0 +1,18 @@
+"""
+UZMCCC Holding - Geleceğe Dönük İşçi ve Hiyerarşi Yönetimi (v2.6)
+"""
+
+class HierarchyManager:
+    def __init__(self):
+        self.departments = {
+            "social_media": ["InstagramUzmani", "TikTokUzmani", "YouTubeUzmani", "FacebookUzmani", "TwitterUzmani"],
+            "communication": ["TelegramYayincisi", "WhatsAppOtomasyoncusu"],
+            "production": ["CanvaTasarimcisi", "CapCutKurgucusu"],
+            "devops": ["GitHubDevOpsUzmani"]
+        }
+
+    def get_department_workers(self, dept_name):
+        return self.departments.get(dept_name, [])
+
+    def validate_command_authority(self, worker_name, command):
+        return True
